edd753f2d898c80a9cbaa59bafd8b4c6f8e83a83
Marayed <marayed0@gmail.com>
2023-03-30 01:40:15 +0100

    creer echange

---
+++ src/Form/EchangeType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Echange;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EchangeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date_echange')
+            ->add('liv_etat')
+            ->add('titre_echange')
+            ->add('archived')
+            ->add('id_user1')
+            ->add('id_user2')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Echange::class,
+        ]);
+    }
+}
