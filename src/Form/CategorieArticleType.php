462056b1817d3aafe585f360714d0303f7c55ddd
Zeldariam <37988307+Zeldariam@users.noreply.github.com>
2023-04-03 03:42:09 +0100

    categorie article crud

---
+++ src/Form/CategorieArticleType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\CategorieArticle;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategorieArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle_cat')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => CategorieArticle::class,
+        ]);
+    }
+}
