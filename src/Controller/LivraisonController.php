f2652e8581f6486ffd1c6840b229a678ba55e809
Marayed <marayed0@gmail.com>
2023-04-09 06:03:01 +0100

    backoffice/echange/livraison

---
+++ src/Controller/LivraisonController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LivraisonController extends AbstractController
+{
+    #[Route('/livraison', name: 'app_livraison')]
+    public function index(): Response
+    {
+        return $this->render('livraison/index.html.twig', [
+            'controller_name' => 'LivraisonController',
+        ]);
+    }
+}
