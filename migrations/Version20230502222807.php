17512455ea249d167d8a10371eb3292014818da8
Oussamabouriga <mohamedbouriga.bouriga@esprit.tn>
2023-05-03 01:10:14 +0100

    final

--- migrations/Version20230502222807.php
+++ migrations/Version20230502222807.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230501172927 extends AbstractMigration
+final class Version20230502222807 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -35,7 +35,8 @@ final class Version20230501172927 extends AbstractMigration
         $this->addSql('CREATE TABLE livraison (id INT AUTO_INCREMENT NOT NULL, id_livreur_id INT DEFAULT NULL, id_echange_id INT DEFAULT NULL, date_creation_livraison DATE DEFAULT NULL, etat_livraison VARCHAR(10) NOT NULL, adresse_livraison1 VARCHAR(255) DEFAULT NULL, adresse_livraison2 VARCHAR(255) DEFAULT NULL, date_terminer_livraison DATE DEFAULT NULL, archived TINYINT(1) DEFAULT NULL, INDEX IDX_A60C9F1F5DEEE7D6 (id_livreur_id), INDEX IDX_A60C9F1FB6FBB8CF (id_echange_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE reclamation (id INT AUTO_INCREMENT NOT NULL, id_user_id INT DEFAULT NULL, titre_reclamation VARCHAR(255) DEFAULT NULL, description_reclamation VARCHAR(500) DEFAULT NULL, etat_reclamation VARCHAR(10) DEFAULT NULL, date_creation DATE DEFAULT NULL, date_cloture DATE DEFAULT NULL, archived TINYINT(1) DEFAULT NULL, INDEX IDX_CE60640479F37AE5 (id_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE reponse (id INT AUTO_INCREMENT NOT NULL, id_reclamation_id INT DEFAULT NULL, titre_reponse VARCHAR(255) DEFAULT NULL, description_reponse VARCHAR(255) DEFAULT NULL, date_reponse DATE DEFAULT NULL, archived TINYINT(1) DEFAULT NULL, avis VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_5FB6DEC7100D1FDF (id_reclamation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, google_authenticator_secret VARCHAR(255) DEFAULT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, nom VARCHAR(255) DEFAULT NULL, prenom VARCHAR(255) DEFAULT NULL, adresse VARCHAR(255) DEFAULT NULL, avatar_url VARCHAR(255) DEFAULT NULL, score INT DEFAULT NULL, archived TINYINT(1) DEFAULT NULL, date_naissance DATE DEFAULT NULL, UNIQUE INDEX UNIQ_1D1C63B3E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, google_authenticator_secret VARCHAR(255) DEFAULT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, nom VARCHAR(255) DEFAULT NULL, prenom VARCHAR(255) DEFAULT NULL, adresse VARCHAR(255) DEFAULT NULL, avatar_url VARCHAR(255) DEFAULT NULL, image_size INT DEFAULT NULL, updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', score INT DEFAULT NULL, archived TINYINT(1) DEFAULT NULL, date_naissance DATE DEFAULT NULL, UNIQUE INDEX UNIQ_1D1C63B3E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE view_items (itemid INT NOT NULL, userid INT NOT NULL, PRIMARY KEY(itemid)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E669F34925F FOREIGN KEY (id_categorie_id) REFERENCES categorie_article (id)');
@@ -58,6 +59,7 @@ final class Version20230501172927 extends AbstractMigration
         $this->addSql('ALTER TABLE livraison ADD CONSTRAINT FK_A60C9F1FB6FBB8CF FOREIGN KEY (id_echange_id) REFERENCES echange (id)');
         $this->addSql('ALTER TABLE reclamation ADD CONSTRAINT FK_CE60640479F37AE5 FOREIGN KEY (id_user_id) REFERENCES utilisateur (id)');
         $this->addSql('ALTER TABLE reponse ADD CONSTRAINT FK_5FB6DEC7100D1FDF FOREIGN KEY (id_reclamation_id) REFERENCES reclamation (id)');
+        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES utilisateur (id)');
     }
 
     public function down(Schema $schema): void
@@ -83,6 +85,7 @@ final class Version20230501172927 extends AbstractMigration
         $this->addSql('ALTER TABLE livraison DROP FOREIGN KEY FK_A60C9F1FB6FBB8CF');
         $this->addSql('ALTER TABLE reclamation DROP FOREIGN KEY FK_CE60640479F37AE5');
         $this->addSql('ALTER TABLE reponse DROP FOREIGN KEY FK_5FB6DEC7100D1FDF');
+        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
         $this->addSql('DROP TABLE article');
         $this->addSql('DROP TABLE article_ratings');
         $this->addSql('DROP TABLE authors');
@@ -98,6 +101,7 @@ final class Version20230501172927 extends AbstractMigration
         $this->addSql('DROP TABLE livraison');
         $this->addSql('DROP TABLE reclamation');
         $this->addSql('DROP TABLE reponse');
+        $this->addSql('DROP TABLE reset_password_request');
         $this->addSql('DROP TABLE utilisateur');
         $this->addSql('DROP TABLE view_items');
         $this->addSql('DROP TABLE messenger_messages');
