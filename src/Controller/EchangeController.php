c75d970acab4ca99d7b00c2c3774934956a66745
Marouan Ayed <63425421+Marayed@users.noreply.github.com>
2023-05-13 05:11:26 +0100

    Update EchangeController.php

--- src/Controller/EchangeController.php
+++ src/Controller/EchangeController.php
@@ -19,6 +19,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
+use App\Repository\UtilisateurRepository;
 
 class EchangeController extends AbstractController
 {
